ab3c215ac1871466766f7012f091559a7f892cd5
Sai Sumanth Vadla <104812692+Saisumanthv@users.noreply.github.com>
2026-02-19 18:32:39 +0530

    Updated app layout

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ load_dotenv()
 
 with st.sidebar:
     st.header("Chat with Sarvam AI")
+    user_name = st.text_input("Enter your name", label_visibility="hidden", placeholder="Enter your name")
     
     selected = option_menu(
         menu_title=None,  
@@ -37,20 +38,36 @@ with st.sidebar:
         }
     )
 
-SARVAM_API_KEY = st.secrets.get("SARVAM_API_KEY") or os.getenv("SARVAM_API_KEY")
-OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY") or os.getenv("OPENAI_API_KEY")
+try:
+    SARVAM_API_KEY = st.secrets.get("SARVAM_API_KEY")
+    OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY")
+except:
+    SARVAM_API_KEY = os.getenv("SARVAM_API_KEY")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if selected == "Text to Speech":
     st.title("Text to Speech:")
-    st.write("Convert your text into speech")
+    # st.write("Convert your text into speech")
     
     sarvam_client = SarvamAI(api_subscription_key=SARVAM_API_KEY,)
     openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
-    user_input = st.text_input("Enter the text to be converted to Speech")
+    col1, col2 = st.columns([3, 1]) 
+
+    with col1:
+        user_input = st.text_input(
+            "", 
+            placeholder="1. Enter the text to be converted to Speech",
+            label_visibility="collapsed"
+        )
+    
+        speech_response_container = st.container()
+
     # user_input = user_input.lower()
 
-    if st.button("Generate Speech"):
+    with col2:
+        generate_speech_button = st.button("Generate Speech", type="primary")
+    if generate_speech_button:
         if user_input:
             response = sarvam_client.text_to_speech.convert(
                 text=user_input,
@@ -65,12 +82,27 @@ if selected == "Text to Speech":
             audio_base64 = response.audios[0]
             audio_bytes = base64.b64decode(audio_base64)
 
-            st.audio(audio_bytes, format="audio/wav")
+            with speech_response_container:
+                st.audio(audio_bytes, format="audio/wav")
 
-    user_query = st.text_input("Enter your query")
-    if st.button("Ask AI"):
+    col3, col4 = st.columns([3, 1])
+
+    with col3:
+        user_query = st.text_input(
+            "", 
+            placeholder="2. Enter your query",
+            label_visibility="collapsed"
+        )
         if user_query:
-
+            if user_name:
+                st.write(f"**{user_name}**" + ": " + user_query)
+            else:
+                st.write("**User:** " + user_query)
+        ai_response_container = st.container()
+    with col4:
+        ask_ai_button = st.button("Ask AI", type="primary")
+    if ask_ai_button:
+        if user_query:
             ai_response = openai_client.chat.completions.create(
                 model="gpt-4o",
                 messages=[
@@ -90,12 +122,14 @@ if selected == "Text to Speech":
                 model="bulbul:v3"
             )
 
+            
             audio_base64 = audio_response.audios[0]
             audio_bytes = base64.b64decode(audio_base64)
-            
+                
             if audio_bytes:
-                st.markdown(text_response)
-                st.audio(audio_bytes, format="audio/wav")
+                with ai_response_container:
+                    st.markdown(f"**AI:** {text_response}")
+                    st.audio(audio_bytes, format="audio/wav")
 
 
 
